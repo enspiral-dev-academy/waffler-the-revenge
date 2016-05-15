ca3d9a6a608227e6043649a3e8bf41c814c7f258
Rich Churcher <rich.churcher@gmail.com>
2016-05-15 21:33:44 +1200

    Switch to octonode

--- push.js
+++ push.js
@@ -1,14 +1,30 @@
-import github from 'github'
+import github from 'octonode'
+
+function getTeam (client, cohort, cb) {
+  client.org(cohort)
+    .teams((err, teams) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      cb(teams.find((t) => {
+        return t.slug === cohort
+      }))
+    })
+}
 
 function push (sprint, cohort) {
-  //const token = process.env['WTR_OAUTH_TOKEN']
-  //var github = new GitHubApi({
-    //version: "3.0.0",
-    //debug: false,
-    //protocol: "https",
-  //})
+  if (!process.env['WTR_OAUTH_TOKEN']) {
+    console.error('Please set WTR_OAUTH_TOKEN')
+    return
+  }
 
-  console.log(`Pushing sprint ${sprint} assignments to ${cohort}...`)
+  const client = github.client(process.env['WTR_OAUTH_TOKEN']);
+
+  getTeam(client, cohort, (team) => {
+    console.log(team)
+  })
+  console.log(`Pushing sprint ${sprint} assignments to \`${cohort}\`...`)
 }
 
 export default push
