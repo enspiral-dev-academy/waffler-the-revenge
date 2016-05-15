d5c33c3708aa1d8baaeb08a553c43a6f4cb56fe8
Rich Churcher <rich.churcher@gmail.com>
2016-05-15 22:12:45 +1200

    Get team membership

--- push.js
+++ push.js
@@ -4,13 +4,39 @@ function getTeam (client, cohort, cb) {
   client.org(cohort)
     .teams((err, teams) => {
       if (err) {
-        console.error(err)
+        console.error(`Couldn't get teams for cohort \`${cohort}\`.`)
         return
       }
       cb(teams.find((t) => {
         return t.slug === cohort
       }))
     })
+}
+
+function getTeamMembers (client, team, cb) {
+  client.team(team)
+    .members((err, members) => {
+      if (err) {
+        console.error(`Couldn't get members for the cohort team.`)
+        return
+      }
+      cb(members.map((member) => {
+        return member.login
+      }))
+    })
+}
+
+function isStudent (client, team, username) {
+  return new Promise((resolve, reject) => {
+    client.team(team)
+      .getMembership(username, (err, status) => {
+        if (err) {
+          console.error(`Couldn't check user \`${username}\`'s role in the team.`)
+          return reject()
+        }
+        return resolve(status)
+      })
+  })
 }
 
 function push (sprint, cohort) {
@@ -22,7 +48,21 @@ function push (sprint, cohort) {
   const client = github.client(process.env['WTR_OAUTH_TOKEN']);
 
   getTeam(client, cohort, (team) => {
-    console.log(team)
+    if (!team) {
+      console.error(`Can't find a team named \`${cohort}\` on org \`${cohort}\`.`)
+      return
+    }
+    getTeamMembers(client, team.id, (members) => {
+      //members = members.filter((member) => {
+        //return isStudent(member)
+      //})
+      members.forEach((member) => {
+        Promise.all(isStudent(client, team.id, member))
+          .then((filtered) => {
+            console.log(filtered)
+          }
+      })
+    })
   })
   console.log(`Pushing sprint ${sprint} assignments to \`${cohort}\`...`)
 }
