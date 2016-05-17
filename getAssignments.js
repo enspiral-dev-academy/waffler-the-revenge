23172410f5bb10b6e1cf49b5c016be80e5a07a79
Rich Churcher <rich.churcher@gmail.com>
2016-05-18 06:40:14 +1200

    Check assignments

--- getAssignments.js
+++ getAssignments.js
@@ -13,6 +13,8 @@ function matchSprint (sprint, assignment) {
 }
 
 function getAssignmentFile (assignment, next) {
+  const client = github.client(process.env['WTR_OAUTH_TOKEN']);
+
   return new Promise((resolve, reject) => {
     client.repo('dev-academy-programme/curriculum-private')
       .contents(`${assignment}/README.md`, (err, file) => {
@@ -25,10 +27,25 @@ function getAssignmentFile (assignment, next) {
 }
 
 function getAssignmentFiles (assignments) {
-  const client = github.client(process.env['WTR_OAUTH_TOKEN']);
   return Promise.resolve(assignments)
   //return Promise.all([
   //])
+}
+
+// Take an array of assignment.path and check to be sure it isn't all just
+// 'p' assignments, which are generic to all sprints
+function checkAssignments (assignments) {
+  return new Promise((resolve, reject) => {
+    const numericOnly = assignments.reject((path) => {
+      const name = path.split('/').pop()
+      return isNaN(parseInt(name[0]))
+    })
+
+    if (numericOnly.length === 0) {
+      return reject(new Error(`Couldn't find any assignments for that sprint.`))
+    }
+    return resolve (assignments)
+  })
 }
 
 function getAssignmentList (sprint) {
@@ -41,7 +58,7 @@ function getAssignmentList (sprint) {
           return reject(new Error(`Couldn't get the assignments from the curriculum repo.`))
         }
         if (assignments.length === 0) {
-          return reject(new Error(`No assignments found for that sprint.`))
+          return reject(new Error(`No assignments found in that repo.`))
         }
 
         const sprintAssignments = assignments
@@ -64,6 +81,7 @@ function sortAndProcess (assignments) {
 function getAssignments (sprint) {
   console.log(`Getting assignments...`)
   return getAssignmentList(sprint)
+    .then(checkAssignments)
     .then(getAssignmentFiles)
     .then(sortAndProcess)
 }
