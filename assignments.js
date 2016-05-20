5897021f1e5d74c016131ed37bbd833f3b4994b4
Rich Churcher <rich.churcher@gmail.com>
2016-05-20 16:12:13 +1200

    More mocking :(

--- assignments.js
+++ assignments.js
@@ -3,57 +3,9 @@ import github from 'octonode'
 export function get (sprint) {
   console.log('Getting assignments...')
   return getList(sprint)
-    .then(check)
+    .then(checkList)
     .then(getFiles)
     .then(sortAndProcess)
-}
-
-function matchSprint (sprint, assignment) {
-  if (assignment.split('-')[0] === 'p') {
-    return true
-  }
-  const prefix = assignment.split('.')[0]
-  const n = parseInt(prefix, 10)
-  if (isNaN(n)) {
-    return false
-  }
-  return sprint === n
-}
-
-export function getFile (assignment, next) {
-  const client = github.client(process.env['WTR_ACCESS_TOKEN'])
-
-  return new Promise((resolve, reject) => {
-    client.repo('dev-academy-programme/curriculum-private')
-      .contents(`${assignment}/README.md`, (err, file) => {
-        if (err) {
-          return reject(new Error(`Couldn't get assignment content for ${assignment}`))
-        }
-        return resolve(file)
-      })
-  })
-}
-
-export function getFiles (assignments) {
-  return Promise.resolve(assignments)
-// return Promise.all([
-// ])
-}
-
-// Take an array of assignment.path and check to be sure it isn't all just
-// 'p' assignments, which are generic to all sprints
-export function check (assignments) {
-  return new Promise((resolve, reject) => {
-    const numericOnly = assignments.filter((path) => {
-      const name = path.split('/').pop()
-      return !isNaN(parseInt(name[0]))
-    })
-
-    if (numericOnly.length === 0) {
-      return reject(new Error("Couldn't find any assignments for that sprint."))
-    }
-    return resolve(assignments)
-  })
 }
 
 export function getList (sprint) {
@@ -68,20 +20,67 @@ export function getList (sprint) {
         if (assignments.length === 0) {
           return reject(new Error('No assignments found in that repo.'))
         }
-
-        const sprintAssignments = assignments
-          .filter((assignment) => {
-            return matchSprint(sprint, assignment.name)
-          })
-          .map((assignment) => {
-            return assignment.path
-          })
-
-        return resolve(sprintAssignments)
+        return resolve(sprintPaths(assignments, sprint))
       })
   })
 }
 
+// Take an array of assignment.path and check to be sure it isn't all just
+// 'p' assignments, which are generic to all sprints
+export function checkList (assignments) {
+  return new Promise((resolve, reject) => {
+    const numericOnly = assignments.filter((path) => {
+      const name = path.split('/').pop()
+      return !isNaN(parseInt(name[0]))
+    })
+
+    if (numericOnly.length === 0) {
+      return reject(new Error('No assignments for that sprint.'))
+    }
+    return resolve(assignments)
+  })
+}
+
+export function getFiles (assignments) {
+  return Promise.all(assignments.map(getFile))
+}
+
 export function sortAndProcess (assignments) {
   return assignments
+}
+
+function getFile (assignment) {
+  const client = github.client(process.env['WTR_ACCESS_TOKEN'])
+
+  return new Promise((resolve, reject) => {
+    client.repo('dev-academy-programme/curriculum-private')
+      .contents(`${assignment}/README.md`, (err, file) => {
+        if (err) {
+          return reject(new Error(`Couldn't get assignment content for ${assignment}`))
+        }
+        return resolve(file)
+      })
+  })
+}
+
+function matchSprint (sprint, assignment) {
+  if (assignment.split('-')[0] === 'p') {
+    return true
+  }
+  const prefix = assignment.split('.')[0]
+  const n = parseInt(prefix, 10)
+  if (isNaN(n)) {
+    return false
+  }
+  return sprint === n
+}
+
+function sprintPaths (assignments, sprint) {
+  return assignments
+    .filter((assignment) => {
+      return matchSprint(sprint, assignment.name)
+    })
+    .map((assignment) => {
+      return assignment.path
+    })
 }
--- test/assignments.test.js
+++ test/assignments.test.js
@@ -3,7 +3,8 @@ import nock from 'nock'
 
 import * as assignments from '../assignments'
 import assignmentList from './json/assignmentList.json'
-import assignmentFile from './json/assignmentFile.json'
+import waffleFile from './json/waffleFile.json'
+import checkinFile from './json/checkinFile.json'
 
 const org = 'dev-academy-programme'
 const repo = 'curriculum-private'
@@ -17,7 +18,9 @@ test('setup', (t) => {
     .get(`/repos/${org}/${repo}/contents/${folder}?access_token=1`)
     .reply(200, assignmentList)
     .get(`/repos/${org}/${repo}/contents/${folder}/1.0-how-to-waffle/README.md?access_token=1`)
-    .reply(200, assignmentFile)
+    .reply(200, waffleFile)
+    .get(`/repos/${org}/${repo}/contents/${folder}/p-check-ins/README.md?access_token=1`)
+    .reply(200, checkinFile)
   t.end()
 })
 
@@ -32,9 +35,7 @@ test('gets assignments for sprint', (t) => {
 test('gets list of paths for sprint', (t) => {
   const expected = [
     'assignments/1.0-how-to-waffle',
-    'assignments/p-check-ins',
-    'assignments/p-read-overview',
-    'assignments/p-weekly-practice'
+    'assignments/p-check-ins'
   ]
   return assignments.getList(1)
     .then((actual) => {
@@ -42,10 +43,28 @@ test('gets list of paths for sprint', (t) => {
     })
 })
 
-test('check errors appropriately on nonexistent sprint', (t) => {
+test('assignments.checkList rejects on nonexistent sprint', (t) => {
   return assignments.getList(12)
     .then((list) => {
-      return t.shouldFail(assignments.check(list), Error)
+      return t.shouldFail(assignments.checkList(list), Error)
+    })
+})
+
+test('assignments.getFiles retrieves a list of files', (t) => {
+  const expected = [
+    {
+      title: '1.0 How to waffle? Click the number in the top left of this card...',
+      body: 'IyAxLjAgSG93IHRvIHdhZmZsZT8gQ2xpY2sgdGhlIG51bWJlciBpbiB0aGUg\ndG9wIGxlZnQgb2YgdGhpcyBjYXJkLi4uCgojIyBXZWxjb21lIHRvIHlvdXIg\nZmlyc3QgdGVzdCBhc3NpZ25tZW50IQoKLSBbIF0gWW91J3JlIHJlYWR5IHRv\nIHN0YXJ0IHdvcmsgb24gYW4gYXNzaWdubWVudD8gRHJhZyBpdCB0byB0aGUg\nJ0luIHByb2dyZXNzJyBjb2x1bW4uCi0gWyBdIFRoaXMgaXMgYSB0YXNrLiBj\naGVjayBpdCB3aGVuIGl0J3MgY29tcGxldGUuIFlvdSBjYW4gY2hlY2sgdGhp\ncyBvbmUgbm93LiBOaWNlLgotIFsgXSBXaGVuIGFsbCB0aGUgdGFza3MgYXJl\nIGNoZWNrZWQsIG1vdmUgdGhlIGFzc2lnbm1lbnQgdG8gdGhlICdSZXZpZXcn\nIGNvbHVtbiEgRG8gdGhhdCBub3cuIEF3ZXNvbWUuCi0gV2UnbGwgY2hlY2sg\neW91ciB3b3JrIHRoZW4gbW92ZSBpdCB0byB0aGUgJ0RvbmUnIGNvbHVtbiB3\naGVuIHdlJ3JlIHNhdGlzZmllZC4KLSBPay4gT250byB0aGUgcmVhbCBhc3Np\nZ25tZW50cyBub3cuLi4gaGF2ZSBmdW4hCg==\n'
+    },
+    {
+      title: 'Check in ~ 30 mins',
+      body: 'IyBDaGVjayBpbiB+IDMwIG1pbnMKPkZhY2UtdGltZSBpcyBpbXBvcnRhbnQg\ndG9vCgotIFsgXSBBdHRlbmQgYSB2aWRlbyBjaGVjay1pbiB3aXRoIHlvdXIg\nY29ob3J0IGZhY2lsaXRhdG9yIDpkZWNpZHVvdXNfdHJlZTogKlRoaXMgaXMg\nYW4gaW1wb3J0YW50IGNoZWNrLWluLCAtIHlvdSBhcmUgZXhwZWN0ZWQgdG8g\nYXR0ZW5kIG9uZSBjaGVjay1pbiBwZXIgd2VlayAoeW91J3JlIHdlbGNvbWUg\ndG8gYm90aCkuIElmIHlvdSBtaXNzIGJvdGggd2Vla2x5IGNoZWNraW5zLCBw\nbGVhc2Ugc3RhdGUgd2h5IGluIGEgd2FmZmxlIGNvbW1lbnQgYmVsb3csIGFu\nZCBzdWJtaXQgdGhpcyBhc3NpZ25tZW50Ki4K\n'
+    }
+  ]
+  return assignments.getList(1)
+    .then(assignments.getFiles)
+    .then((actual) => {
+      t.deepEqual(actual, expected)
     })
 })
 
