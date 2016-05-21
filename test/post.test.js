76266324f170bc68b875bfc50e6579957403b0df
Rich Churcher <rich.churcher@gmail.com>
2016-05-21 18:57:09 +1200

    Posts

---
+++ test/post.test.js
@@ -0,0 +1,49 @@
+import test from 'blue-tape'
+import nock from 'nock'
+
+import * as post from '../post'
+
+const cohort = 'waffler-test'
+
+process.env['WTR_ACCESS_TOKEN'] = 1
+
+test('mock API responses', (t) => {
+  nock('https://api.github.com')
+    .persist()
+    .get(`/orgs/${cohort}/teams?access_token=1`)
+    .reply(200, teams)
+  t.end()
+})
+
+test('students.getTeam returns the correct team', (t) => {
+  const expected = {
+    slug: 'waffler-test'
+  }
+  return students.getTeam(cohort)
+    .then((actual) => {
+      t.equal(actual.slug, expected.slug)
+    })
+})
+
+test('students.getTeam rejects if no org', (t) => {
+  return t.shouldFail(students.getTeam('___probably_does_not_exist___', Error))
+})
+
+test('students.getTeam rejects if no team', (t) => {
+  return t.shouldFail(students.getTeam(noTeams))
+})
+
+test('students.getTeamMembers returns the correct usernames', (t) => {
+  const team = {
+    id: 1
+  }
+  const expected = ['richchurcher']
+  return students.getTeamMembers(team)
+    .then((actual) => {
+      t.deepEqual(actual, expected)
+    })
+})
+
+test('students.getTeamMembers rejects on an empty team', (t) => {
+  return t.shouldFail(students.getTeam(99))
+})
